4018e702c61f63c4b7f4a129b4884f7ea5b16255
Dalton Maahs <dalton.maahs@yourbind.com>
2020-09-30 13:31:31 -0500

    zsh/oh-my-zsh

---
+++ .oh-my-zsh/custom/exports.zsh
@@ -0,0 +1,17 @@
+# Path Exports
+export PATH="$PATH:$HOME/bin"
+export PATH="$PATH:$HOME/dotfiles/scripts"
+export PATH="$PATH:$HOME/Casks"
+
+# Other Exports
+export FZF_BASE="/usr/local/bin/fzf"
+export EDITOR='vim'
+export TERM="xterm-256color"
+export MD_NOTEBOOKS_DIR="$HOME/dev/md-notebooks"
+
+# brew
+export HOMEBREW_CASK_OPTS='--appdir=~/Casks'
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!! (with zsh)
+export SDKMAN_DIR="$HOME/.sdkman" 
+[[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
