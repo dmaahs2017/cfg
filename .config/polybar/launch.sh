19e4f1921014ea43a234b0a619f62c36594cf3e2
dalton <maahs2017@gmail.com>
2021-10-30 12:23:30 -0500

    update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Add this script to your wm startup file.
+DIR="$HOME/.config/polybar"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -6,5 +9,9 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar example &
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
+
+# IPC settings and test
+ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-main
+echo message >/tmp/ipc-main
