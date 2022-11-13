3dcee5380d97482adda7b2fa47c97c540c47eddf
dalton <maahs2017@gmail.com>
2022-11-13 16:38:52 -0600

    update

--- .oh-my-zsh/custom/exports.zsh
+++ .oh-my-zsh/custom/exports.zsh
@@ -22,3 +22,9 @@ export SDKMAN_DIR="$HOME/.sdkman"
 
 # CUDA
 export LD_LIBRARY_PATH=/opt/cuda/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+
+# node version manager
+[ -z "$NVM_DIR" ] && export NVM_DIR="$HOME/.nvm"
+source /usr/share/nvm/nvm.sh
+source /usr/share/nvm/bash_completion
+source /usr/share/nvm/install-nvm-exec
